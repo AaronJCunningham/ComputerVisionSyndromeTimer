15436e2ada258243558a59e7f94d444f37ac85ce
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2021-04-21 12:53:09 +0200

    css

--- src/components/utils/formatAlarmMessage.js
+++ src/components/utils/formatAlarmMessage.js
@@ -1,13 +1,13 @@
 export const formatAlarmMessage = (timer) => {
   return timer === 0 ? (
-    <h1>Press Start</h1>
+    <h1 className="alarm">Press Start</h1>
   ) : timer >= 1200 ? (
     timer >= 1500 ? (
-      <h1>Break</h1>
+      <h1 className="alarm">Break</h1>
     ) : (
-      <h1>Alarm</h1>
+      <h1 className="alarm">Alarm</h1>
     )
   ) : (
-    <h1>Working</h1>
+    <h1 className="alarm">Working</h1>
   );
 };
